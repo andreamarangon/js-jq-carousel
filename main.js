a93dfd6d21eddf0720ebd99aed262da563a2b8d7
Andrea Marangon <marangonandrea84@gmail.com>
2021-03-11 16:25:26 +0100

    .

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
   avanti();
+
 });
+
 
 function avanti(){
   var nextFoto = $('.next .fa-angle-right');
   var prevFoto = $('.prev .fa-angle-left');
   var active = $('.active')
   nextFoto.click(function(){
-    active.removeClass('active');
-    active.next().addClass('active');
+    active.removeClass('active').next().addClass('active');
+
   });
 
 };
