4334a04f17db196f081e8a3046b510cdb00e17eb
Andrea Marangon <marangonandrea84@gmail.com>
2021-03-11 17:55:06 +0100

    aggiunta di funzionamento frecce avanti/indietro

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
   avanti();
-
+  indietro();
 });
 
 
 function avanti(){
   var nextFoto = $('.next .fa-angle-right');
-  var prevFoto = $('.prev .fa-angle-left');
-  var active = $('.active')
   nextFoto.click(function(){
+    var active = $('.active')
     active.removeClass('active').next().addClass('active');
+  });
+
+};
+
+function indietro(){
+  var prevFoto = $('.prev .fa-angle-left');
+  prevFoto.click(function(){
+    var active = $('.active')
+    active.removeClass('active').prev().addClass('active');
 
   });
 
