526eb9bb4bd041365ee8fb8029f723016ae81c21
Andrea Marangon <marangonandrea84@gmail.com>
2021-03-11 15:49:07 +0100

    inizio esercizio

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  avanti();
+});
+
+function avanti(){
+  var nextFoto = $('.next .fa-angle-right');
+  var prevFoto = $('.prev .fa-angle-left');
+  var active = $('.active')
+  nextFoto.click(function(){
+    active.removeClass('active');
+    active.next().addClass('active');
+  });
+
+};
